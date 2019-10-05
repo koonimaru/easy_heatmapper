a940da91378d76f9fce9f74fabf3cd783f4279b1
koh <koh@koh-Inspiron-7590>
2019-10-05 11:45:34 +0900

    ...

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import easy_heatmapper as eh
 b=np.random.normal(0,1, size=(25,25))
-    for i in range(10):
-        b=np.concatenate((b, np.random.normal(i+1, 1, size=(25,25) )), axis=0)
+for i in range(10):
+    b=np.concatenate((b, np.random.normal(i+1, 1, size=(25,25) )), axis=0)
 eh.heatmapper(b)
